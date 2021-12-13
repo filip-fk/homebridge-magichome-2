0d612600ca255ccb54f08e7dc64eceb50a8117ad
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-13 09:39:14 +0300

    Fixed script path

--- src/lib/lightAgent.js
+++ src/lib/lightAgent.js
@@ -138,7 +138,7 @@ const LightAgent = class {
 
   getDevices() {
     const self = this
-    const cmd = path.join(__dirname, '../flux_led.py')
+    const cmd = '/usr/bin/env ' + 'flux_led'
     self.log('Discovering Devices')
     this.proc = spawn(cmd, ['-s'])
     this.proc.stdout.on('data', (data) => {
