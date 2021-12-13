176e1bbfe7bb209160e3a9153fbed4e1d9eabc31
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-13 09:28:59 +0300

    Final fix

--- src/accessories/base.js
+++ src/accessories/base.js
@@ -36,7 +36,7 @@ const Accessory = class {
   executeCommand(address, command, callback) {
     const exec = cp.exec
     const self = this
-    const cmd = '/usr/bin/env ', 'flux_led ' + lightAgent.getAddress(address) + command
+    const cmd = '/usr/bin/env ' + 'flux_led ' + lightAgent.getAddress(address) + command
     if (self.homebridge.debug) {
       self.log(cmd)
     }
